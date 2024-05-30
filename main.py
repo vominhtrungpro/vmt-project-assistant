e65300af5ba0d427542fcfc157bd05e5ac88b325
Trung Vo <vominhtrungpro@gmail.com>
2024-05-30 14:58:08 +0700

    deploy

--- main.py
+++ main.py
@@ -74,7 +74,7 @@ def create_thread():
 @app.route('/api/run',methods=['POST'])
 def create_run():    
     if not os.environ.get('is_assistant_enable') == 'True':
-        return Response("Currently Offline!", mimetype='text/plain')
+        return Response("Currently offline!", mimetype='text/plain')
     
     data = request.get_json()  
     thread_id = data.get('thread_id')
